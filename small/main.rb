20c263e8c10318137283442f3fac1eee8fb24e05
arkneca <stojanpro@gmail.com>
2016-11-06 22:31:14 +0100

    Create tests

--- small/main.rb
+++ small/main.rb
@@ -1,7 +1,7 @@
 require 'sinatra'
 
 class String
-  
+
   def caesar_shift(shift=1)
     letters = ("a".."z").to_a
     ciphertext = []
@@ -36,3 +36,40 @@ post '/' do
   @ciphertext = @plaintext.caesar_shift(shift)
   erb :result
 end
+
+##### TESTS ######
+
+if ARGV.include? 'test'
+
+  set :environment, :test
+  set :run, false
+
+  require 'test/unit'
+  require 'rack/test'
+
+
+  class CaesarCipherTest < Test::Unit::TestCase
+    include Rack::Test::Methods
+
+    def app
+      Sinatra::Application
+    end
+
+    def test_it_can_encrypt_strings
+      assert_equal 'JGNNQ','hello'.caesar_shift(2)
+    end
+
+    def test_it_can_encrypt_with_negative_shifts
+      assert_equal 'GDKKN','hello'.caesar_shift(-1)
+    end
+
+    def test_it_can_encrypt_from_a_URL
+      post '/', params={plaintext: 'hello', shift: '2'}
+      assert last_response.ok?
+      assert last_response.body.include?('hello'.caesar_shift(2))
+    end
+
+  end
+
+end
+
